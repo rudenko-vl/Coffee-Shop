e25bd66e24452c8c7bc6ac5b74aee4aa28a7a608
Руденко  Владислав Сергійович <rvs07@i.ua>
2024-01-25 11:41:57 +0200

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 const search = document.querySelector('.search-box');
 const searchBtn = document.getElementById('search-icon');
+const navbar = document.querySelector('.navbar');
+const menuIcon = document.getElementById('menu-icon');
 
 searchBtn.addEventListener('click', () => {
     search.classList.toggle('active')
+    navbar.classList.remove('active')
+})
+
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active')
+    search.classList.remove('active')
+})
+
+window.onscroll = () => {
+    navbar.classList.remove('active')
+    search.classList.remove('active')
+}
+
+let header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
 })
